6a274a663000504b4c2078cff2bff19233a1db0a
kodyclemens <kody.clemens@gmail.com>
2018-12-19 16:46:47 -0500

    User signup implemented, redirects to homepage after account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   get '/signup' do
     erb :'/users/signup'
   end
+
+  post '/signup' do
+    user = User.create(params)
+    session[:id] = user.id
+    redirect '/'
+  end
 end
