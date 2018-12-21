d3cad44a24a3ef45256e28553cfe3ff8fb67e1a9
kodyclemens <kody.clemens@gmail.com>
2018-12-21 16:28:06 -0600

    added roles to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181219224434) do
+ActiveRecord::Schema.define(version: 20181221222616) do
 
   create_table "jobs", force: :cascade do |t|
     t.string  "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20181219224434) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
+    t.string "role",            default: "user"
   end
 
 end
