1394389fe2912a81b68d982c11aa5fd2ede061cb
kodyclemens <kody.clemens@gmail.com>
2018-12-22 16:07:48 -0600

    admin role created, admin view added and users_controller refactored

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 class UsersController < ApplicationController
   get '/signup' do
-    @logged_in = false
     erb :'/users/signup'
   end
   
   get '/logout' do
     session.clear
-    @logged_in = false
     redirect '/'
   end
 
   get '/login' do
     if logged_in?
-      @logged_in = true
       flash[:blah]
       redirect '/'
     else
-      @logged_in = false
       erb :'/users/login'
+    end
+  end
+
+  get '/admin' do
+    if logged_in?
+      erb :'/users/admin'
+    else
+      redirect '/'
     end
   end
 
