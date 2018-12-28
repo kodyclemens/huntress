a0231230397ef8efafdde36c757c8021c7888c15
kodyclemens <kody.clemens@gmail.com>
2018-12-28 11:56:39 -0600

    Scraper added to grab job attributes from Indeed through user inputted URL.

--- app/services/job_scraper.rb
+++ app/services/job_scraper.rb
@@ -1,3 +1,14 @@
-class JobScraper
+require './config/environment'
 
+class JobScraper
+  def ScrapeIndeed(job_post_url)
+    doc = Nokogiri::HTML(open(job_post_url))
+    # doc = Nokogiri::HTML(open("https://www.indeed.com/viewjob?jk=020b29cbee251c60"))
+    # doc = Nokogiri::HTML(open("https://www.indeed.com/cmp/The-Doyle-Group/jobs/Lead-Software-Engineer-1ee43d45c20a79bc"))
+    title = doc.css(".jobsearch-DesktopStickyContainer h3").text
+    company = doc.css(".icl-u-lg-mr--sm")[0].text
+    location = doc.css(".jobsearch-DesktopStickyContainer .icl-u-xs-mt--xs .jobsearch-InlineCompanyRating").text.split("-")[1]
+    description = doc.css(".jobsearch-JobComponent-description").text
+    Job.new(title: title, company: company, location: location, description: description)
+  end
 end
